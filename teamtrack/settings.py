677ba633a8c8365be7a832227434d3085a2a4a3d
Ansh Wasan <69054054+FrostyFeet-coder@users.noreply.github.com>
2025-06-24 15:50:45 +0530

    updated security

--- teamtrack/settings.py
+++ teamtrack/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7at8d*%a9_3%arq&e#!k%ntajv_$58fs3zb$!@6#rg_#=qlexx'
+import os
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
